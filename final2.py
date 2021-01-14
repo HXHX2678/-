c0e70dd6e963a6ed7d5753dea479a633decfbfcd
HXHX2678 <62589247+HXHX2678@users.noreply.github.com>
2021-01-14 10:30:25 +0800

    Update final2.py

--- final2.py
+++ final2.py
@@ -16,19 +16,13 @@ mydoc=collection.find().sort("_id",-1)#1.2.3.4.5 to 5.4.3.2.1
 
 count=0
 while 1:
-        #db.collection_names(include_system_collections=False)
         print(mydoc[0])
-        #pprint.pprint(collection.find_one({"author": "Jian"}))
         line = collection.find_one({"author": "Final"})
-        #regular = re.findall("'distance':[0-9]+",str(mydoc[0]))
-        #regular=(line["distance"])
         regular = re.findall(r"'distance': \d+.\d+",str(mydoc[0]))
-        #print('hi')
+        
         regularV2=re.findall(r"\d+.\d+",str(regular))
         print(regular)
-        #regularV2=re.findall("[0-9]+",str(regular))
-        #print(regularV2[0])
         d=regularV2[0]
-        publish.single('final/Jian',d,hostname='163.13.136.72',auth={'username':'DLRobot','password':'ff21ff21'})
+        publish.single('final/Jian',d,hostname='xxx.xxxx.xx.xxx',auth={'username':'DLRobot','password':'xxxxxx'})
         count=count+1
         time.sleep(1.5)
